f451a3132c08fa0074f99ee2352d0eb41b27c6d5
Jack Seal <jseal7@gatech.edu>
2024-12-16 00:57:12 -0500

    Comments & Fixed Readability Syntax
    Fixed variables to always use camel case and finished adding docstrings to all functions. Additionally added in-line comments to GUI implementation.

--- OutlookContactScript.py
+++ OutlookContactScript.py
@@ -16,29 +16,29 @@ def getOutlookContacts():
 
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     contacts = outlook.GetDefaultFolder(10).Items
-    contacts_list = []
+    contactsList = []
 
     for contact in contacts:
-        contact_info = {}
+        contactInfo = {}
 
-        contact_info['Name'] = contact.FullName
+        contactInfo['Name'] = contact.FullName
 
-        contact_info['Email1'] = contact.Email1Address
-        contact_info['Email2'] = contact.Email2Address
-        contact_info['Email3'] = contact.Email3Address
+        contactInfo['Email1'] = contact.Email1Address
+        contactInfo['Email2'] = contact.Email2Address
+        contactInfo['Email3'] = contact.Email3Address
 
-        contact_info['Business'] = contact.BusinessTelephoneNumber
-        contact_info['Home'] = contact.HomeTelephoneNumber
-        contact_info['Mobile'] = contact.MobileTelephoneNumber
+        contactInfo['Business'] = contact.BusinessTelephoneNumber
+        contactInfo['Home'] = contact.HomeTelephoneNumber
+        contactInfo['Mobile'] = contact.MobileTelephoneNumber
 
-        contact_info['Address'] = contact.MailingAddress
+        contactInfo['Address'] = contact.MailingAddress
 
-        contact_info['Company'] = contact.CompanyName
-        contact_info['Job Title'] = contact.JobTitle
+        contactInfo['Company'] = contact.CompanyName
+        contactInfo['Job Title'] = contact.JobTitle
         
-        contacts_list.append(contact_info)
+        contactsList.append(contactInfo)
 
-    return contacts_list
+    return contactsList
 
 def createExceltemplate():
     """
@@ -59,18 +59,35 @@ def createExceltemplate():
     except Exception as e:
         tkinter.messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
-def makeExcel(contacts_list = None, filename = 'outlook_contacts.xlsx', template = False):
+
+def makeExcel(contactsList = None, fileName = 'outlook_contacts.xlsx', template = False):
+    """
+    Makes an excel sheet and populates it with contacts if it is not marked as a template.
+
+    Args:
+        contactsList: A list of dictionaries containing contacts information. Default set to None.
+        fileName: The file name for excel sheet. Default set to 'outlook_contacts.xlsx'.
+        template: Boolean for if the excel sheet with be a template or not. Default set to False.
+
+    Returns:
+        messagebox: Creates a formatted excel sheet and returns its success status, the file name, and where the file was stored.
+
+    Raises:
+        Exception: If any error occurs that prevents making a new excel sheet.
+    """
+
     try:
         excelBook = Workbook()
         currSheet = excelBook.active
         currSheet.title = "Outlook Contacts"
         currSheet.append(['Name', 'Email1', 'Email2', 'Email3', 'Business Phone', 'Home Phone', 'Mobile Phone', 'Address', 'Company', 'Job Title'])
         
-        if contacts_list and not template:
-            for contact in contacts_list:
-                currSheet.append([contact['Name'], contact['Email1'], contact['Email2'], contact['Email3'], contact['Business'], contact['Home'], contact['Mobile'], contact["Address"], contact['Company'], contact['Job Title']])
+        if contactsList and not template:
+            for contact in contactsList:
+                currSheet.append([contact['Name'], contact['Email1'], contact['Email2'], contact['Email3'], contact['Business'],
+                                contact['Home'], contact['Mobile'], contact["Address"], contact['Company'], contact['Job Title']])
         
-        excelBook.save(filename)
+        excelBook.save(fileName)
 
         if not template:
             fileLocation = os.getcwd()
@@ -80,21 +97,36 @@ def makeExcel(contacts_list = None, filename = 'outlook_contacts.xlsx', template
     except Exception as e:
         tkinter.messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
-def makeContacts(filename, contacts_list):
-    if not os.path.exists(filename):
+def makeContacts(fileName, contactsList):
+    """
+    Creates contacts in Outlook Contact Book from list of contacts information.
+
+    Args:
+        contactsList: A list of dictionaries containing contacts information.
+        fileName: The file name for excel sheet.
+
+    Returns:
+        messagebox: Creates Outlook contacts and returns its success status and the file name. If a file of the same name already exists,
+                    it returns a message to start with a template.
+
+    Raises:
+        Exception: If any error occurs that prevents making a new excel sheet.
+    """
+
+    if not os.path.exists(fileName):
         fileName = 'outlook_contacts.xlsx'
         tkinter.messagebox.showinfo("No Excel Sheet", f"No file {fileName} exists to populate contacts. Create a \'Template\' and fill in contact info first!")
         return
 
-    excelBook = load_workbook(filename)
+    excelBook = load_workbook(fileName)
     currSheet = excelBook.active
 
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     contacts = outlook.GetDefaultFolder(10).Items
 
     nameArr = []
-    for i in range(len(contacts_list)):
-        nameArr.append(contacts_list[i]['Name'])
+    for i in range(len(contactsList)):
+        nameArr.append(contactsList[i]['Name'])
     
     for i in range(2, currSheet.max_row + 1):
         if (not (currSheet.cell(row= i, column= 1).value in nameArr)):
@@ -126,24 +158,26 @@ def makeContacts(filename, contacts_list):
     fileName = 'outlook_contacts.xlsx'
     tkinter.messagebox.showinfo("Success", f"Outlook Contacts were populated from file {fileName}!")
 
-#Function to create the GUI Window for usability.
 def makeGui():
+    """Makes a Graphical User Interface for using the scripts functions."""
+
+    # Establishing of the intial outer GUI window. Instatiates windows size and makes in unresizable.
     guiWindow = customtkinter.CTk(fg_color='#49332b')
     guiWindow.title("Excel to Outlook Contact")
     guiWindow.geometry("250x315")
     guiWindow.resizable(False, False)
     guiWindow.attributes("-fullscreen", False)
 
+    # Creating and configuring the internal box that holds the GUI's content.
     frame = customtkinter.CTkFrame(guiWindow, fg_color='#edd8bc')
     frame.pack(fill="both", expand=True, padx=10, pady=10)
-
     frame.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
     frame.grid_columnconfigure(0, weight=1)
 
-    #Create a base directory for where the script is running
+    # Create a base directory for where the script is running
     baseDirectory = os.path.dirname(os.path.abspath(__file__))
 
-    #Create the two image paths corresponding to the images for each scripts buttons.
+    # Create the two image paths corresponding to the images for each scripts buttons.
     excelToContactImagePath = os.path.join(baseDirectory, "images", "contact-book.png")
     excelToContactImagePath = excelToContactImagePath.replace("\\", "/")
 
@@ -156,12 +190,13 @@ def makeGui():
     quitImagePath = os.path.join(baseDirectory, "images", "quit.png")
     quitImagePath = quitImagePath.replace("\\", "/")
     
-    #Changing and resizing into usable image.                       
+    # Changing and resizing into usable image.                       
     excelToContactImage = customtkinter.CTkImage(light_image=Image.open(excelToContactImagePath), size=(48, 48))
     contactToExcelImage = customtkinter.CTkImage(light_image=Image.open(contactToExcelImagePath), size=(48, 48))
     templateImage = customtkinter.CTkImage(light_image=Image.open(templateImagePath), size=(48, 48))
     quitImage = customtkinter.CTkImage(light_image=Image.open(quitImagePath), size=(48, 48))
 
+    # Title and Underline (first row start)
     scriptTitle = customtkinter.CTkLabel(
         frame,
         text="Contact Functions",
@@ -176,7 +211,9 @@ def makeGui():
         height=2,
         width=200)
     titleUnderline.grid(row=1, column=0, sticky="n")
+    # (first row end)
 
+    # Top two function buttons (second row start)
     secondRow = customtkinter.CTkFrame(frame, fg_color="#edd8bc")
     secondRow.grid(row=2, column=0)
 
@@ -207,7 +244,9 @@ def makeGui():
         cursor = 'hand2',
         command = lambda: makeExcel(getOutlookContacts(), 'outlook_contacts.xlsx'))
     outlookToExcelButton.pack(side=tkinter.LEFT, padx=10, pady=(7,0))
+    # (second row end)
 
+    # First two labels depicting button functions (third row start)
     thirdRow = customtkinter.CTkFrame(frame, fg_color="#edd8bc")
     thirdRow.grid(row=3, column=0)
 
@@ -228,7 +267,9 @@ def makeGui():
         text_color='#33231e',
         font=('Arial', 12, 'bold'))
     outlookToExcelLabel.pack(side=tkinter.LEFT, padx=17, pady=(0,10))
+    # (third row end)
 
+    # Second row of buttons (fourth row)
     fourthRow = customtkinter.CTkFrame(frame, fg_color="#edd8bc")
     fourthRow.grid(row=4, column=0)
 
@@ -259,7 +300,9 @@ def makeGui():
         cursor='hand2',
         command=guiWindow.quit)
     quitButton.pack(side=tkinter.LEFT, padx=10, pady=(7,0))
+    # (fourth row end)
 
+    # Second row of labels depicting the functions of the buttons above them. (fifth row start)
     fifthRow = customtkinter.CTkFrame(frame, fg_color="#edd8bc")
     fifthRow.grid(row=5, column=0)
 
@@ -280,9 +323,9 @@ def makeGui():
         text_color='#33231e',
         font=('Arial', 12, 'bold'))
     quitLabel.pack(side=tkinter.LEFT, padx=17, pady=(0,10))
+    # (fifth row end)
 
     guiWindow.mainloop()
-
 
 if __name__ == "__main__":
     desktopPath = os.path.join(os.environ['USERPROFILE'], 'Desktop')
